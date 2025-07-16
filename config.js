9cc64be50a2aac0a9a4682b1c829b66e5e60862c
jin1963 <kaojino89@gmail.com>
2025-07-17 02:22:47 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+const tokenContracts = {
+  KJC: {
+    address: "0xd479ae350dc24168e8db863c5413c35fb2044ecd",
+    decimals: 18,
+  },
+  G3X24: {
+    address: "0x6cfD8Fe423F20F94825b5edB1E94068fBea19dC9",
+    decimals: 18,
+  },
+  LYDIA: {
+    address: "0xbfD941B45f6E9850Ba82510284426dFD3fBf25E2", // 👈 ที่คุณส่งมา
+    decimals: 18,
+  }
+};
